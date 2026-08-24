6dc7038aa518f0c20ebd97c431570c62863d717b
gosenjenbe <post@jonas-fleer.de>
2026-08-24 23:13:48 +0200

    Remove predefined word lists from word_generator.py
    Removed predefined lists of 'also_not_words' and 'not_words' from the word_generator.py file.

--- word_generator.py
+++ word_generator.py
@@ -1,31 +1,6 @@
 import random
 from urllib.request import urlopen
 
-also_not_words = ['CHEWBACCA', 'CAR', 'HOUSE']
-
-not_words = [
-    'APPLE',
-    'BREAD',
-    'CHAIR',
-    'DREAM',
-    'EARTH',
-    'FLAME',
-    'GRACE',
-    'HEART',
-    'IMAGE',
-    'JUICE',
-    'KNIFE',
-    'LIGHT',
-    'MUSIC',
-    'OCEAN',
-    'PLANT',
-    'QUEEN',
-    'RIVER',
-    'SMILE',
-    'TRAIN',
-    'WATER'
-    ]
-
 word_source = "https://www.mit.edu/~ecprice/wordlist.10000"
 
 with urlopen(word_source) as response:
